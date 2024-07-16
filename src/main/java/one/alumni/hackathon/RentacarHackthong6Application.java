f36b4eb03429d7ad3e2835ab163a8174d3578d99
Jose Bonilla <bonillasanchez98@gmail.com>
2024-07-16 13:49:48 -0300

    first commit

---
+++ src/main/java/one/alumni/hackathon/RentacarHackthong6Application.java
@@ -0,0 +1,13 @@
+package one.alumni.hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentacarHackthong6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentacarHackthong6Application.class, args);
+	}
+
+}
