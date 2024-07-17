2959ea8dc0f7fef7f1c15f133861e9c70e089440
Daniel Amaya <49620375+daldev14@users.noreply.github.com>
2024-07-17 10:58:59 -0600

    Revert "Signup customer "

--- src/main/java/one/alumni/hackathon/repository/BookACarRepository.java
+++
@@ -1,9 +0,0 @@
-package one.alumni.hackathon.repository;
-
-import one.alumni.hackathon.entity.BookACar;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookACarRepository extends JpaRepository<BookACar, Long> {
-}
