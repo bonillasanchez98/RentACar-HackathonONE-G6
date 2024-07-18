2c285966fcb8c2ab0bc91425b5ea096a22171cb2
Daniel Amaya <damayalop@gmail.com>
2024-07-17 22:29:53 -0600

    Remove component

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter as FontSans } from 'next/font/google'
 import './globals.css'
 
 import { cn } from '@/lib/utils'
-import Header from '@/components/landing-page/Header'
 
 const fontSans = FontSans({
   subsets: ['latin'],
