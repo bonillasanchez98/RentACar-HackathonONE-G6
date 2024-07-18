fb53a498f9840c8c644f9e46e7589e6369b79aa7
Daniel Amaya <damayalop@gmail.com>
2024-07-17 19:04:59 -0600

    Add BaseLayout component

--- client/app/(auth)/signin/page.tsx
+++ client/app/(auth)/signin/page.tsx
@@ -9,55 +9,58 @@ import {
 } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import BaseLayout from '@/components/BaseLayout'
 
 export default function page() {
   return (
-    <main className='grid place-content-center py-16'>
-      <Card className='mx-auto max-w-sm'>
-        <CardHeader>
-          <CardTitle className='text-2xl'>Login</CardTitle>
-          <CardDescription>
-            Introduzca su dirección de correo electrónico para acceder a su
-            cuenta
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <div className='grid gap-4'>
-            <div className='grid gap-2'>
-              <Label htmlFor='email'>Email</Label>
-              <Input
-                id='email'
-                type='email'
-                placeholder='john.doe@mail.com'
-                required
-              />
-            </div>
-            <div className='grid gap-2'>
-              <div className='flex items-center'>
-                <Label htmlFor='password'>Password</Label>
-                <Link
-                  href='#'
-                  className='hidden ml-auto .inline-block text-sm underline'>
-                  Forgot your password?
-                </Link>
+    <BaseLayout>
+      <main className='grid place-content-center py-16'>
+        <Card className='mx-auto max-w-sm'>
+          <CardHeader>
+            <CardTitle className='text-2xl'>Login</CardTitle>
+            <CardDescription>
+              Introduzca su dirección de correo electrónico para acceder a su
+              cuenta
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className='grid gap-4'>
+              <div className='grid gap-2'>
+                <Label htmlFor='email'>Email</Label>
+                <Input
+                  id='email'
+                  type='email'
+                  placeholder='john.doe@mail.com'
+                  required
+                />
               </div>
-              <Input id='password' type='password' required />
+              <div className='grid gap-2'>
+                <div className='flex items-center'>
+                  <Label htmlFor='password'>Password</Label>
+                  <Link
+                    href='#'
+                    className='hidden ml-auto .inline-block text-sm underline'>
+                    Forgot your password?
+                  </Link>
+                </div>
+                <Input id='password' type='password' required />
+              </div>
+              <Button type='submit' className='w-full'>
+                Login
+              </Button>
+              <Button variant='outline' className='hidden w-full'>
+                Login with Google
+              </Button>
             </div>
-            <Button type='submit' className='w-full'>
-              Login
-            </Button>
-            <Button variant='outline' className='hidden w-full'>
-              Login with Google
-            </Button>
-          </div>
-          <div className='mt-4 text-center text-sm'>
-            Don&apos;t have an account?{' '}
-            <Link href='/signup' className='underline'>
-              Sign up
-            </Link>
-          </div>
-        </CardContent>
-      </Card>
-    </main>
+            <div className='mt-4 text-center text-sm'>
+              Don&apos;t have an account?{' '}
+              <Link href='/signup' className='underline'>
+                Sign up
+              </Link>
+            </div>
+          </CardContent>
+        </Card>
+      </main>
+    </BaseLayout>
   )
 }
---
+++ client/components/BaseLayout.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './landing-page/Header'
+
+export default function BaseLayout({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <>
+      <div className='absolute top-0 z-[-2] h-screen w-full bg-white bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]'></div>
+      <Header />
+      {children}
+    </>
+  )
+}
