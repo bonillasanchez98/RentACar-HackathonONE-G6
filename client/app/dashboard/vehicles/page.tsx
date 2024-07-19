d4b6df3c17ca75a4ccc6518ba64b979a50364883
Daniel Amaya <49620375+daldev14@users.noreply.github.com>
2024-07-19 20:32:20 +0000

    Add DialogViewCar component

--- client/app/dashboard/vehicles/page.tsx
+++ client/app/dashboard/vehicles/page.tsx
@@ -1,18 +1,12 @@
-import { PlusCircle } from 'lucide-react'
-import { Button } from '@/components/ui/button'
 import DataTable from '@/components/dashboard/vehicle/data-table-vehicles/DataTable'
+import DialogViewCar from '@/components/dashboard/vehicle/dialog-view-car/DialogViewCard'
 
 export default function page() {
   return (
     <div className='w-full'>
       <div className='flex justify-end mb-3'>
         <div className='ml-auto flex items-center gap-2'>
-          <Button size='sm' className='h-8 gap-1'>
-            <PlusCircle className='h-3.5 w-3.5' />
-            <span className='sr-only sm:not-sr-only sm:whitespace-nowrap'>
-              Add Vehicle
-            </span>
-          </Button>
+          <DialogViewCar />
         </div>
       </div>
       <DataTable />
