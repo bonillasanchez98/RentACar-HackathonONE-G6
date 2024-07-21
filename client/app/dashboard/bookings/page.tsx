f47a8e381360b9994a774393659457429f69ea4a
Daniel Amaya <damayalop@gmail.com>
2024-07-21 00:13:58 -0600

    Add bookings page

---
+++ client/app/dashboard/bookings/page.tsx
@@ -0,0 +1,14 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+
+export default function page() {
+  return (
+    <div className='w-full'>
+      <Card>
+        <CardHeader className='px-7'>
+          <CardTitle>Reservas</CardTitle>
+        </CardHeader>
+        <CardContent></CardContent>
+      </Card>
+    </div>
+  )
+}
