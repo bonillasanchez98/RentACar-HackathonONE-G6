875c28bd7e51c83eba106bd349deeddb3cb38c03
Daniel Amaya <damayalop@gmail.com>
2024-07-21 01:30:59 -0600

    Add react-hook-form dependency

--- client/app/(auth)/signin/page.tsx
+++ client/app/(auth)/signin/page.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link'
-import { Button } from '@/components/ui/button'
 import {
   Card,
   CardContent,
@@ -7,9 +6,8 @@ import {
   CardHeader,
   CardTitle
 } from '@/components/ui/card'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
 import BaseLayout from '@/components/BaseLayout'
+import SignInForm from './SignInForm'
 
 export default function page() {
   return (
@@ -24,34 +22,7 @@ export default function page() {
             </CardDescription>
           </CardHeader>
           <CardContent>
-            <div className='grid gap-4'>
-              <div className='grid gap-2'>
-                <Label htmlFor='email'>Email</Label>
-                <Input
-                  id='email'
-                  type='email'
-                  placeholder='john.doe@mail.com'
-                  required
-                />
-              </div>
-              <div className='grid gap-2'>
-                <div className='flex items-center'>
-                  <Label htmlFor='password'>Password</Label>
-                  <Link
-                    href='#'
-                    className='hidden ml-auto .inline-block text-sm underline'>
-                    Forgot your password?
-                  </Link>
-                </div>
-                <Input id='password' type='password' required />
-              </div>
-              <Button type='submit' className='w-full'>
-                Login
-              </Button>
-              <Button variant='outline' className='hidden w-full'>
-                Login with Google
-              </Button>
-            </div>
+            <SignInForm />
             <div className='mt-4 text-center text-sm'>
               Don&apos;t have an account?{' '}
               <Link href='/signup' className='underline'>
--- client/app/(auth)/signup/page.tsx
+++ client/app/(auth)/signup/page.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { Button } from '@/components/ui/button'
+
 import {
   Card,
   CardContent,
@@ -7,15 +7,14 @@ import {
   CardHeader,
   CardTitle
 } from '@/components/ui/card'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
 import BaseLayout from '@/components/BaseLayout'
+import SignUpForm from './SignUpForm'
 
 export default function LoginForm() {
   return (
     <BaseLayout>
       <main className='grid place-content-center py-16'>
-        <Card className='mx-auto max-w-sm'>
+        <Card className='mx-auto'>
           <CardHeader>
             <CardTitle className='text-xl'>Sign Up</CardTitle>
             <CardDescription>
@@ -23,37 +22,7 @@ export default function LoginForm() {
             </CardDescription>
           </CardHeader>
           <CardContent>
-            <div className='grid gap-4'>
-              <div className='grid grid-cols-2 gap-4'>
-                <div className='grid gap-2'>
-                  <Label htmlFor='first-name'>First name</Label>
-                  <Input id='first-name' placeholder='Max' required />
-                </div>
-                <div className='grid gap-2'>
-                  <Label htmlFor='last-name'>Last name</Label>
-                  <Input id='last-name' placeholder='Robinson' required />
-                </div>
-              </div>
-              <div className='grid gap-2'>
-                <Label htmlFor='email'>Email</Label>
-                <Input
-                  id='email'
-                  type='email'
-                  placeholder='john.doe@mail.com'
-                  required
-                />
-              </div>
-              <div className='grid gap-2'>
-                <Label htmlFor='password'>Password</Label>
-                <Input id='password' type='password' />
-              </div>
-              <Button type='submit' className='w-full'>
-                Create an account
-              </Button>
-              <Button variant='outline' className='hidden w-full'>
-                Sign up with GitHub
-              </Button>
-            </div>
+            <SignUpForm />
             <div className='mt-4 text-center text-sm'>
               Already have an account?{' '}
               <Link href='/signin' className='underline'>
