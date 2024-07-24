a5615ad2afef26078c9939d5753270d52dc83fc5
Daniel Amaya <damayalop@gmail.com>
2024-07-23 21:10:48 -0600

    Update schemas

--- client/lib/zod.ts
+++ client/lib/zod.ts
@@ -27,7 +27,7 @@ export const signUpSchema = z.object({
   password: z
     .string({ required_error: 'El campo es requerido' })
     .min(1, 'El campo es requerido')
-    .min(8, 'La contraseña debe tener más de 8 caracteres')
+    .min(3, 'La contraseña debe tener más de 8 caracteres')
     .max(32, {
       message: 'El contraseña debe tener menos 32 caracteres'
     })
