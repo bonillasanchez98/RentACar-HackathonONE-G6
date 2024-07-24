a64edb7ba97f616d6c0feb40e57d90de835a6bdd
Daniel Amaya <damayalop@gmail.com>
2024-07-23 20:30:05 -0600

    Add customers pages

---
+++ client/app/(protected)/dashboard/vehicles/page.tsx
@@ -0,0 +1,12 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+
+export default function page() {
+  return (
+    <Card>
+      <CardHeader className='px-7'>
+        <CardTitle>Vehiculos</CardTitle>
+      </CardHeader>
+      <CardContent>{}</CardContent>
+    </Card>
+  )
+}
